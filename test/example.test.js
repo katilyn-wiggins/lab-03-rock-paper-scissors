228694b3f7ea399934c2f4d4b84003bbbddbc60e
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-01-14 00:24:01 -0500

    CSS updated

--- test/example.test.js
+++ test/example.test.js
@@ -1,36 +1,23 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
-
 import { getRandomThrow } from '../get-random-throw.js';
 import { doesUserWin } from '../utils.js';
 
 const test = QUnit.test;
 
 test('if passed 0, should return rock', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
+
     const expected = 'rock';
 
-    //Act 
-    // Call the function you're testing and set the result to a const
     const actual = getRandomThrow(0);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
 
 test('if passed 1, should return paper', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
+
     const expected = 'paper';
 
-    //Act 
-    // Call the function you're testing and set the result to a const
     const actual = getRandomThrow(1);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
 
